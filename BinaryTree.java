e37ece5c338cdc0149fa68898428ce2a3f8f2d54
shadowasphodel2919 <t2905j@gmail.com>
2022-07-24 22:23:27 +0530

    Added binary tree java template

---
+++ BinaryTree.java
@@ -0,0 +1,16 @@
+public class BinaryTree {
+    static class Node{
+        int val;
+        Node left, right;
+        Node(int value){
+            this.val = value;
+            left = right = null;
+        }
+    }
+
+    public static void main(String[] args){
+        BinaryTree btree = new BinaryTree();
+        Node root = new Node(5);
+        
+    }
+}
